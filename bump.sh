1a8830221812ef8850d0936a96d7ed80c18614a9
johnerick-py <johnerick-py@users.noreply.github.com>
2023-03-13 10:52:51 -0300

    add

---
+++ bump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y git
+apt-get install -y rsync
+git remote rm origin
+git config --global user.name "$GITHUB_USER"
+git config --global user.email "$GITHUB_EMAIL"
+git config --global credential.helper 'store --file $HOME/.my-credentials'
+git config --global credential.helper cache
+git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git
+# rm -rf .git/
+# rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec rm -rf {} \; 
+cd $GITHUB_REPO_NAME
+rm -rf .git/
+git init
+git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git
+git checkout -b main
+git remote -v
+git add .
+git commit -m "[skip CI]"
+git push -f origin main
+
+
