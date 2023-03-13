791194492fe9d8480733a677fb8acb35ff6a76dc
johnerick-py <johnerick-py@users.noreply.github.com>
2023-03-13 15:45:06 -0300

    add2

--- bump.sh
+++ bump.sh
@@ -7,18 +7,21 @@ git config --global user.name "$GITHUB_USER"
 git config --global user.email "$GITHUB_EMAIL"
 git config --global credential.helper 'store --file $HOME/.my-credentials'
 git config --global credential.helper cache
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git
-
+#git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git
+cd $GITHUB_REPO_NAME
 pwd
 ls
-# rm -rf .git/
+git pull
+rm -rf ./git
+cd ..
+#rm -rf .git/
 # rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec rm -rf {} \; 
 #rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec mv {} $GITHUB_REPO_NAME/ \; && rm -rf ./*
 find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path ./$GITHUB_REPO_NAME | xargs -I{} mv {} $GITHUB_REPO_NAME/
 
 
 cd $GITHUB_REPO_NAME
-rm -rf .git/
+#rm -rf .git/
 pwd
 ls
 git init
