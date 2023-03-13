1be1a49b0f6fc089a1c9d23d96b691ff6ca53bf5
johnerick-py <johnerick-py@users.noreply.github.com>
2023-03-13 14:38:15 -0300

    add2

--- bump.sh
+++ bump.sh
@@ -10,9 +10,14 @@ git config --global credential.helper cache
 git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git
 
 pwd
+ls
 # rm -rf .git/
 # rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec rm -rf {} \; 
-rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec mv {} $GITHUB_REPO_NAME/ \; && rm -rf ./*
+#rsync -av --exclude='$GITHUB_REPO_NAME' ./ $GITHUB_REPO_NAME/ && find . -maxdepth 1 ! -name $GITHUB_REPO_NAME ! -path $GITHUB_REPO_NAME -exec mv {} $GITHUB_REPO_NAME/ \; && rm -rf ./*
+mv -v !($GITHUB_REPO_NAME) $GITHUB_REPO_NAME/
+shopt -s extglob # Ativa a extensão glob
+rm -rf !($GITHUB_REPO_NAME|<$GITHUB_REPO_NAME>) # Apaga tudo exceto "x" e o repositório Git
+
 cd $GITHUB_REPO_NAME
 rm -rf .git/
 pwd
